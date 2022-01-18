9520b3054fddc48e1c559981db2b8edde5188a00
aaaslikyan <42067484+aaaslikyan@users.noreply.github.com>
2022-01-19 01:08:29 +0400

    Update C__Users_aaasl_OneDrive_Desktop_ABC.py

--- C__Users_aaasl_OneDrive_Desktop_ABC.py
+++ C__Users_aaasl_OneDrive_Desktop_ABC.py
@@ -25,7 +25,7 @@ n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
 
-@st.cache(ttl=24*60*60)
+@st.cache
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
